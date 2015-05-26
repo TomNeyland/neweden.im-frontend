079f2e3ec1e883c9a2b06c55e009e3480498df7d
Tom Neyland <tcneyland+github@gmail.com>
2015-05-25 23:14:41 -0400

    refactor(example): Updates code style for module/controller creation

--- app/example/index.js
+++ app/example/index.js
@@ -1,11 +1,67 @@
-import angular from 'angular';
+import 'angular-aria';
+import 'angular-material';
 import 'angular-ui-router';
+import angular from 'angular';
+import _ from 'lodash';
 
-import ExampleState from './example';
+/*
+	Module Definition
+ */
 
-export default angular.module('example', [
+const exampleModule = angular.module('example', [
     'ui.router',
     'ngMaterial'
-])
+]);
 
-.config(ExampleState);
+
+/*
+	Controller Definitions
+ */
+
+class ExampleCtrl {
+    constructor($scope, data) {
+        this.exampleData = data;
+    }
+
+    someMethod() {
+        return _.map(this.exampleData, function(data) {
+            return (data * 2);
+        });
+    }
+
+    get data() {
+        return this.exampleData;
+    }
+}
+
+ExampleCtrl.$inject = ['$scope', 'data'];
+
+
+/*
+	Controller Registration
+*/
+
+exampleModule.controller('ExampleCtrl', ExampleCtrl);
+
+
+/*
+	State definitions
+ */
+
+exampleModule.config(['$stateProvider', function($stateProvider) {
+    $stateProvider.state('example', {
+        controller: 'ExampleCtrl',
+        controllerAs: 'Example',
+        url: '/example',
+        template: require('./_example.html'),
+        resolve: {
+            data: [function() {
+                return [1, 2, 3, 4, 5, 6];
+            }]
+        }
+    });
+}]);
+
+
+// Export the module
+export default exampleModule;
--- app/example/index.spec.js
+++ app/example/index.spec.js
@@ -1,31 +1,31 @@
-import {ExampleCtrl} from './example';
-import './index';
+import 'angular';
+import 'angular-mocks';
+import './';
 
-describe('Example module', function() {
-    var $controllerProvider, scope = {}, $rootScope, $controller;
+
+describe('example', function() {
 
     beforeEach(angular.mock.module('example'));
 
-    beforeEach(angular.mock.module(function(_$controllerProvider_) {
-        $controllerProvider = _$controllerProvider_;
-    }));
+    describe('ExampleCtrl', function() {
 
-    describe('Help controller', function() {
+        var scope;
+        var ExampleCtrl;
 
-        beforeEach(angular.mock.inject(function($injector) {
-            $rootScope = $injector.get('$rootScope');
-            $controller = $injector.get('$controller');
-            $controllerProvider.register('ExampleCtrl', ExampleCtrl);
-
+        beforeEach(inject(function($rootScope, $controller) {
             scope = $rootScope.$new();
-
-            $controller('ExampleCtrl as Example', {
+            ExampleCtrl = $controller('ExampleCtrl as Example', {
                 $scope: scope,
                 data: [1, 2, 3, 4, 5, 6]
             });
         }));
 
+        it('should be available on scope as Example', function() {
+            return expect(scope.Example).to.equal(ExampleCtrl);
+        });
+
         it('should load example data', function() {
+            console.log(scope.Example);
             expect(scope.Example.exampleData).to.not.be.undefined;
         });
 
@@ -41,5 +41,7 @@ describe('Example module', function() {
         it('should have a method called someMethod that returns the double for each value', function() {
             expect(scope.Example.someMethod()).to.deep.equal([2, 4, 6, 8, 10, 12]);
         });
+
     });
+
 });
